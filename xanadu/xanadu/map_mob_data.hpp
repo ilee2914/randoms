3fba9c968e1c8706893d746826a88a7313847726
Ira Lee <ilee2914@gmail.com>
2018-04-15 20:12:21 -0400

    Initial Import
    Fixed flying drops
    Bosses now spawn correctly

---
+++ xanadu/xanadu/map_mob_data.hpp
@@ -0,0 +1,15 @@
+//
+
+#pragma once
+
+// plain old data struct
+
+struct MapMobData
+{
+	bool flip;
+	short position_x;
+	short position_y;
+	short foothold;
+	int mob_id;
+	int mobTime;
+};
---
+++ xanadu/xanadu/scripts/npcs/default.as
@@ -0,0 +1,3 @@
+void main(Player @player) {
+	player.send_ok("NPC not coded.");
+}
